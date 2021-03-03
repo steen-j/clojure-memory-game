5b2766fc28043816cecdaf86c64d87abe46e1b9a
Steen Jensen <steen.erikjost.jensen@ufst.dk>
2021-03-03 13:02:59 +0100

    Added mini-app function

--- src/main/memorygame/app.cljs
+++ src/main/memorygame/app.cljs
@@ -3,8 +3,11 @@
     [reagent.core :as r]
     [reagent.dom :as rdom]))
 
+(defn miniapp []
+      [:<>])
+; Herunder ligger funktionerne til at starte det hele op. Dem behøver I ikke bekymre jer om i første omgang
 (defn ^:export run []
-      (rdom/render [:<>] (js/document.getElementById "app")))
+      (rdom/render [miniapp] (js/document.getElementById "app")))
 
 (defn ^:export reload []
       (js/console.log "reload...")
